e8bab399b82800e7b3d86347ac316d4ed8404369
Doridian <git@doridian.net>
2024-10-28 18:34:23 -0700

    fixups

--- keyboards/framework/matrix.c
+++ keyboards/framework/matrix.c
@@ -264,19 +264,19 @@ bool handle_idle(void) {
     static uint8_t prev_asleep = -1;
     if (prev_asleep != asleep) {
         prev_asleep = asleep;
-    }
-    if (asleep) {
-        led_suspend();
-    } else {
-        led_wakeup();
+        if (asleep) {
+            led_suspend();
+        } else {
+            led_wakeup();
+        }
     }
 #ifdef RGB_MATRIX_ENABLE
     if (rgb_matrix_get_suspend_state() != asleep) {
-        if (asleep) {
-            writePinLow(IS31FL3743A_ENABLE_GPIO);
-        } else {
-            writePinHigh(IS31FL3743A_ENABLE_GPIO);
-        }
+        //if (asleep) {
+        //    writePinLow(IS31FL3743A_ENABLE_GPIO);
+        //} else {
+        //    writePinHigh(IS31FL3743A_ENABLE_GPIO);
+        //}
         rgb_matrix_set_suspend_state(asleep);
     }
 #endif
